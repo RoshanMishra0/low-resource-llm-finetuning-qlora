d3a29f0903a0a54dcf7495ddc79198050caf9365
RoshanMishra0 <roshanmishra2003@gmail.com>
2026-04-13 19:14:27 +0530

    Update tokenizer_utils.py

--- src/tokenizer_utils.py
+++ src/tokenizer_utils.py
@@ -1,1 +1,15 @@
+from transformers import AutoTokenizer
+from config import MODEL_NAME, MAX_LENGTH
 
+def get_tokenizer():
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    tokenizer.pad_token = tokenizer.eos_token
+    return tokenizer
+
+def tokenize_function(example, tokenizer):
+    return tokenizer(
+        example["text"],
+        truncation=True,
+        padding="max_length",
+        max_length=MAX_LENGTH
+    )
